870da561fe0df842fa6101fa4d6af2c43185b59e
Thalia Indah <thaliaindah20@gmail.com>
2026-08-03 09:44:25 +0700

    Update presenter

--- MovieApp/Presenter/MovieListPresenter.swift
+++ MovieApp/Presenter/MovieListPresenter.swift
@@ -11,11 +11,16 @@ import SwiftUI
 final class MovieListPresenter: ObservableObject {
     @Published var movieLists: [MovieListModel] = []
     @Published var isLoading: Bool = false
+    @Published var isLoadMore: Bool = false
     @Published var errorMsg: String?
+    
     let genre: GenreModel
     let router: MovieRouter
     private let interactor: MovieInteractorProtocol
+    
     private var cancellable: Set<AnyCancellable> = []
+    private var currentPage = 1
+    private var totalPage = 1
     
     init(genre: GenreModel, interactor: MovieInteractorProtocol, router: MovieRouter) {
         self.genre = genre
@@ -24,18 +29,42 @@ final class MovieListPresenter: ObservableObject {
     }
     
     func getMovieLists(){
-        isLoading = true
-        errorMsg = nil
-        interactor.fetchMovies(gendreId: genre.id)
+        guard !isLoading else { return }
+        guard !isLoadMore else { return }
+        guard currentPage <= totalPage else { return }
+        if currentPage == 1 {
+            isLoading = true
+        } else {
+            isLoadMore = true
+        }
+        interactor.fetchMovies(page: currentPage, gendreId: genre.id)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] completion in
-                self?.isLoading = false
+                guard let self else { return }
+                self.isLoading = false
+                self.isLoadMore = false
                 if case .failure(let error) = completion {
-                    self?.errorMsg = error.localizedDescription
+                    self.errorMsg = error.localizedDescription
                 }
             } receiveValue: { [weak self] movieLists in
-                self?.movieLists = movieLists
+                guard let self else { return }
+                self.totalPage = movieLists.total_pages
+                if self.currentPage == 1 {
+                    self.movieLists = movieLists.results
+                } else {
+                    let exsistingMovie = Set(
+                        self.movieLists.map(\.id)
+                    )
+                    let newMovies = movieLists.results.filter { !exsistingMovie.contains($0.id) }
+                    self.movieLists.append(contentsOf: newMovies)
+                }
+                self.currentPage += 1
             }
             .store(in: &cancellable)
+    }
+    
+    func loadMoreMovie(movie: MovieListModel) {
+        guard movie.id == movieLists.last?.id else { return }
+        getMovieLists()
     }
 }
